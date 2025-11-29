9f358f398a7c3fd5d000c48bd1a21f3d2ddb2c35
SHRISHTYPANDEY <shrishty1228p@gmail.com>
2025-11-29 19:48:35 +0530

    frontend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,2 +1,6 @@
-const server = "http://localhost:8000";
+let IS_PROD = true;
+
+const server = IS_PROD
+  ? "https://video-call-1-dxpf.onrender.com"
+  : "http://localhost:8000";
 export default server;
